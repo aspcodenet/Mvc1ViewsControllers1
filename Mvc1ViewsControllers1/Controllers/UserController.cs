e507dd414512f5d8590028c900ae5d2ec0990145
Stefan Holmberg <stefan.holmberg@systementor.se>
2021-01-21 16:15:49 +0100

    Forms labb 1

---
+++ Mvc1ViewsControllers1/Controllers/UserController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mvc1ViewsControllers1.ViewModels;
+
+namespace Mvc1ViewsControllers1.Controllers
+{
+    public class UserController : Controller
+    {
+
+        public IActionResult RegisterConfirmation()
+        {
+            return View();
+        }
+
+
+
+        // GET
+        public IActionResult Create()
+        {
+            var viewModel = new UserCreateViewModel();
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Create(UserCreateViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                //Spara nu anv i databasen
+
+                return RedirectToAction("RegisterConfirmation");
+
+            }
+
+            return View(model);
+        }
+
+    }
+}
---
+++ Mvc1ViewsControllers1/ViewModels/UserCreateViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mvc1ViewsControllers1.ViewModels
+{
+    public class UserCreateViewModel
+    {
+        [DataType(DataType.EmailAddress)]
+        public string EmailAddress { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        [DataType(DataType.Password)] // ***
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)] // ***
+        public string ConfirmPassword { get; set; }
+
+        public bool SendMeUpdates { get; set; }
+
+    }
+}
